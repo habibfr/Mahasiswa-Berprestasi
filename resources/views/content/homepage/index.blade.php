c2847860fc0824d53532c036e167cba85c55400b
Excel951 <abigailexcelsisdeo@gmail.com>
2023-12-11 00:00:55 +0700

    add homepage

--- resources/views/content/homepage/index.blade.php
+++ resources/views/content/homepage/index.blade.php
@@ -3,19 +3,64 @@
 @section('title', 'Homepage')
 
 @section('content')
-<div class="container m-3">
-    <div class="row position-relative">
-        <div class="col position-absolute start-0 top-0">
-            <h1 class="">KRILIN</h1>
-        </div>
-        <div class="col position-absolute end-0 top-0">
-            <a href="login" class="btn btn-danger">Login</a>
-        </div>
+<div class="container my-3">
+    <div class="row justify-content-between align-items-start">
+        <div class="col-md-2 text-start"><h1 class="fw-bold">KRILIN</h1></div>
+        <div class="col-md-2 text-end"><a href="/kriteria" class="btn btn-danger fw-bold">LOGIN</a></div>
     </div>
-    <div class="row">
-        <div class="col">
-            <h1>Mahasiswa Berprestasi</h1>
+
+    <div class="container mx-3">
+        <div class="row text-center mb-3"><h1 class="fw-bold">Mahasiswa Berprestasi</h1></div>
+        <div class="row text-end mb-3"><div class="col"><button class="btn btn-secondary">Unduh</button></div></div>
+        <div class="row">
+            <div class="card">
+                <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Poin</th>
+                    </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                    <tr>
+                        <td>1</td>
+                        <td>XXXXXXXXXXX</td>
+                        <td>Muhammad Haris</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>XXXXXXXXXXX</td>
+                        <td>Muhammad Maulana</td>
+                        <td>95</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>XXXXXXXXXXX</td>
+                        <td>Muhammad Kholiq</td>
+                        <td>93</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>XXXXXXXXXXX</td>
+                        <td>Muhammad Abi</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>XXXXXXXXXXX</td>
+                        <td>Muhammad Ansyah</td>
+                        <td>89</td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
+
     </div>
 </div>
 
